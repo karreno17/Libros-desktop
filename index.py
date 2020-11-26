eeab1e53c3a14fbb71c8c3b042691658af581bec
karreno17 <davidcn@ufps.edu.co>
2020-11-25 21:48:18 -0500

    Frame de la app

---
+++ index.py
@@ -0,0 +1,80 @@
+from tkinter import ttk
+from tkinter import *
+
+import sqlite3
+
+class Book:
+    # connection dir property
+    db_name = 'database.db'
+
+    def __init__(self, window):
+        # Initializations 
+        self.wind = window
+        self.wind.title('Aplicacion de libros')
+
+        # Creating a Frame Container 
+        frame = LabelFrame(self.wind, text = 'Register new Book')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        # Name Input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
+
+        # Price Input
+        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+        # Button Add Book 
+        ttk.Button(frame, text = 'Save Book', command = self.add_book).grid(row = 3, columnspan = 2, sticky = W + E)
+
+        # Output Messages 
+        self.message = Label(text = '', fg = 'red')
+        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+
+        # Table
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Name', anchor = CENTER)
+        self.tree.heading('#1', text = 'Price', anchor = CENTER)
+
+        # Buttons
+        ttk.Button(text = 'DELETE', command = self.delete_book).grid(row = 5, column = 0, sticky = W + E)
+        ttk.Button(text = 'EDIT', command = self.edit_book).grid(row = 5, column = 1, sticky = W + E)
+
+        # Filling the Rows
+        self.get_book()
+
+    # Function to Execute Database Querys
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    # Get Book from Database
+    def get_book(self):
+        # cleaning Table 
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        # getting data
+        query = 'SELECT * FROM book ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        # filling data
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
+    # User Input Validation
+    def validation(self):
+        return len(self.name.get()) != 0 and len(self.price.get()) != 0
+
+
+
+if __name__ == '__main__':
+    window = Tk()
+    application = Book(window)
+    window.mainloop()    
